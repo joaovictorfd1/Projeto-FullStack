9867ceba93c5af9f8ab8e44aa691c47ffcb31a73
joaovictorfd1 <joaovictorffd1@gmail.com>
2024-03-22 14:54:34 -0300

    feat: novo link de api

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const apiUrl = process.env.REACT_APP_API || 'https://dummyjson.com/';
-const apiUrl = "https://beta-back-635d2bfec1b2.herokuapp.com"
+const apiUrl = "https://beta-back-4952727c822e.herokuapp.com/"
 
 export const api = axios.create({
   baseURL: apiUrl,
